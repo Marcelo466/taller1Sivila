91b77eda67c0b57524cea8db4ade1971bce47c16
e18184@gmail.com <e18184@gmail.com>
2024-03-20 16:20:53 -0400

    20-03-2024

--- G4/js/objetos.js
+++ G4/js/objetos.js
@@ -1,4 +1,4 @@
-var valor = Number("10ccc");
+/*var valor = Number("10ccc");
 console.log(valor);
 console.log("CADENAS");
 var valor1 = String("1000ccccc");
@@ -8,7 +8,7 @@ console.log("CARACTERES EN UNA CADENA");
 var micadena = "MI NOMBRE ES";
 var cantidad = micadena.length;
 console.log(micadena.toLowerCase()); 
-console.log(cantidad); 
+console.log(cantidad); */
 /*
 var tmascota = {
     nombre: "Terry",
@@ -29,4 +29,44 @@ persona.am + " DIRECCIONES: " + persona.vdireccion[1] + "\n MASCOTA:"+
 persona.mascota.mostrar();
 alert (mensaje);
 */
+/*
+var lista = new Array(1,2,3,4,5,6);
+console.log(lista); 
+console.log(typeof lista);
+lista.push(7);
+lista.push(8);
+lista.push(9);
+console.log(lista);
+var cadena = new String("uno","dos","tres","cuatro","cinco");
+console.log(cadena);
+
+var vector = new String ("Hola esta es una nueva cadena");
+console.log("cadena"+vector);
+resultado = vector.split(" ");
+console.log(resultado);
+*/
+// poner algun texto que identifique que ha sido encontrado el elemento de la lista 1 
+// y el elemento 2
+
+/*lista = document.querySelector("lista1");
+console.log(lista);
+console.log(typeof lista);*/
+//lista2 = document.querySelectorAll("lista1");
+lista2 = document.getElementById("lista1");
+lista2.children[1].outerText = lista2.children[1].outerText+ "ENCONTRADO \n";
+lista2.children[2].outerText = lista2.children[2].outerText+ "ENCONTRADO \n";
+
+
+/*
+console.log(lista2);
+lista2[1].outerText = lista2[1].outerText + "ENCONTRADO \n";
+console.log("elemento:"+ typeof lista2[1])
+lista2[2].outerText = lista2[2].outerText + "ENCONTRADO";
+*/
+
+
+
+
+
+
 
